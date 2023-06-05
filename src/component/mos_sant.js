015b4a13e791465046964764f47c9e7660ee340c
Sriyansh srivastava <sriyansh2001@gmail.com>
2023-06-06 00:55:04 +0530

    bugs fixed

--- src/component/mos_sant.js
+++ src/component/mos_sant.js
@@ -89,14 +89,16 @@ export default function Mos_sant() {
         if(m_boat+s_boat<=0) return ;
         if(state_boat===0) {
             change_state_boat(1);
-            console.log(m_boat,monster_right,s_boat,saint_right);
+            // console.log(m_boat,monster_right,s_boat,saint_right);
             if(m_boat+monster_right > s_boat+saint_right && (saint_right!==0 || s_boat!==0)) {
                 alert("Game Over");
                 reset_all();
+                return ;
             }
             if(monster_left>saint_left && saint_left!==0) {
                 alert("Game Over");
                 reset_all();
+                return ;
             }
             change_place({float:"right"});
             // console.log()
@@ -106,10 +108,12 @@ export default function Mos_sant() {
             if(m_boat+monster_left>s_boat+saint_left && (saint_left!==0 || s_boat!==0)) {
                 alert("Game Over");
                 reset_all();
+                return ;
             }
             if(monster_right>saint_right && saint_right!==0) {
                 alert("Game Over");
                 reset_all();
+                return ;
             }
             // console.log(m_boat,monster_left,s_boat,saint_left,saint_left ,s_boat);
             change_place({float:"left"});
@@ -125,29 +129,44 @@ export default function Mos_sant() {
         change_m_boat(0);
         change_s_boat(0);
         change_state_boat(0);
+        change_place({float:"left"});
     }
 
     return (
         <div>
+            <div className='Rules'>
+                <h2>Description</h2>
+                <ol>
+                    <li>Capacity of boat is 2.</li>
+                    <li>If monster is greater than saint at any end point you will loss the game.</li>
+                    <li>You Need to place all the 6 members to the finish end.</li>
+                </ol>
+                <br></br>
+            </div>
             <div className='main'>
                 <div className="firstend">
+                    <div className='heading'><h2>Start</h2></div><br></br>
                     <button className='btnmonster' onClick={go_moster_boat_left}>Monster :</button> {monster_left} <br></br>
                     <button className='btnsaint' onClick={go_saint_boat_left}>Saint :</button> {saint_left} 
                 </div>
                 <div className="boat">
+                    <div className='heading'><h2>Boat</h2></div><br></br>
                     <div  style={place}>
                         <button className='btnmonster' onClick={go_moster_back_to_ground}> Moster : </button> {m_boat} <br></br>
                         <button className='btnsaint' onClick={go_saint_back_to_ground}>Saint : </button> {s_boat} <br></br>
-                        <button onClick={change_boat_ground}>Pass : </button> {state_boat===0?"Left End":"Right End"}
+                        <button onClick={change_boat_ground}>Pass : </button> {state_boat===0?"Boat on Left End":"Boat on Right End"}
                     </div>
                 </div>
                 <div className="secondend">
+                    <div className='heading'><h2>Finish</h2></div><br></br>
                     <button className='btnmonster' onClick={go_moster_boat_right}>Monster : </button> {monster_right} <br></br>
                     <button className='btnsaint' onClick={go_saint_boat_right}>Saint : </button> {saint_right}
                 </div>
             </div>
-            <button onClick={result}>Check</button>
-            <button onClick={reset_all}>Reset</button>
+            <div className='lower_btn'>
+                <button onClick={result}>Check</button>
+                <button onClick={reset_all}>Reset</button>
+            </div>
         </div>
     )
 }
